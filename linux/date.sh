8f4f2cffaea788c8f881f2262fa24fe68be44cdb
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2022-09-11 01:16:10 +0900

    [update] create linux directory (#2)

---
+++ linux/date.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Ref:
+#https://cmdref.net/os/linux/command/date.html
+
+exit
+
+
+
+date --date '10day ago' +"%Y%m%d" → 20061030
+date +"%Y%m%d" #20061030
+date +"%H:%M" #12:47
+cp -p FILE FILE.`date +%Y%m%d`
+cp -p FILE FILE.`date -d '1day ago' +%Y%m%d`
+
+
